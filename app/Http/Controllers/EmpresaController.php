85ff1e0da4b324311e23e914ef5fe5b858068fe2
Daniel <daniel.holiveira@gmail.com>
2019-03-29 15:32:31 -0300

    Experimento Laravel

---
+++ app/Http/Controllers/EmpresaController.php
@@ -0,0 +1,8 @@
+<?php namespace estoque\Http\Controllers;
+
+class EmpresaController {
+
+    public function lista(){
+        return '<h1>Listagem de produtos com Laravel</h1>';
+    }
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace paineladm\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        // 'paineladm\Model' => 'paineladm\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
